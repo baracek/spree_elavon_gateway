e759062fde61bcb59029a51b2f3ac74f736eb72f
Sathya Sekaran <sfsekaran@gmail.com>
2013-01-27 01:00:39 -0800

    Updating spree_elavon_gateway to be usable with at least spree 1.1.3

--- app/models/gateway/elavon.rb
+++ app/models/gateway/elavon.rb
@@ -1,8 +1,11 @@
-class Gateway::Elavon < Gateway
-      preference :login, :string
-      preference :password, :string
-      preference :user, :string
-   def provider_class
+module Gateway
+  class Elavon < Spree::Gateway
+    preference :login, :string
+    preference :password, :string
+    preference :user, :string
+
+    def provider_class
       ActiveMerchant::Billing::ElavonGateway
-   end
+    end
+  end
 end
--- lib/spree_elavon_gateway.rb
+++ lib/spree_elavon_gateway.rb
@@ -1,22 +1,2 @@
 require 'spree_core'
-require 'spree_elavon_gateway_hooks'
-
-module SpreeElavonGateway
-  class Engine < Rails::Engine
-
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    def self.activate
-      require 'active_merchant'
-      ActiveMerchant::Billing::ElavonGateway
-      
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
-      end
-      
-      Gateway::Elavon.register
-    end
-
-    config.to_prepare &method(:activate).to_proc
-  end
-end
+require 'spree_elavon_gateway/engine'
